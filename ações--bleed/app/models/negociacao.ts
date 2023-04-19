c89a0fc237ad27e66b0cd022a81e0f21e9dc5495
Bleed <raphamf421@gmail.com>
2023-04-18 22:58:58 -0300

    Initial commit

---
+++ ações--bleed/app/models/negociacao.ts
@@ -0,0 +1,16 @@
+export class Negociacao {
+
+    constructor(
+        private _data: Date,
+        public readonly _quantidade: number,
+        public readonly _valor: number){}
+
+    get volume(): number {
+        return this._quantidade * this._valor
+    }
+
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return this._data;
+    }
+}
